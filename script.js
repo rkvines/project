f71b0b40da1cf1f19e9f6ec3a78ad3d5892ee901
Ritesh Thakare <128716512+rkvines@users.noreply.github.com>
2023-09-30 15:58:25 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// hello javascript
+console.log(" Welcome To Infotech Project Page ")
+
+// Any Script This Type TO another
+
+
+
+// Extra Project button to click show hidden project's 
+
+function extra(){
+    document.getElementById('extra-project').style.display = 'block';
+}
+
+// load button function
+
+function load(){
+    document.getElementById('load').style.cursor = 'no-drop' ;
+    document.getElementById('load').innerHTML = 'That IS ALL' ;
+    document.getElementById('load').style.background = 'white' ;
+    document.getElementById('load').style.color = 'black' ;
+}
